973c09ac05c642be4619518c03f6446d0d85b505
ZionFrost <inaki337@gmail.com>
2023-02-27 09:49:13 +0100

    Card Setup
    Added an card Model & an script to play the animations of the card.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : MonoBehaviour, IPointerDownHandler
+{
+    private Animator _animator;
+
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        AddEvents();
+    }
+
+    #region animation events
+    private void AddEvents()
+    {
+        for(int i = 0;i< _animator.runtimeAnimatorController.animationClips.Length; i++)
+        {
+            AnimationClip clip = _animator.runtimeAnimatorController.animationClips[i];
+            Debug.Log(clip.name);
+
+            AnimationEvent animationStartEvent = new AnimationEvent();
+            animationStartEvent.time = 0;
+            animationStartEvent.functionName = "AnimationStartHandler";
+            animationStartEvent.stringParameter = clip.name;
+
+            AnimationEvent animationEndEvent = new AnimationEvent();
+            animationEndEvent.time = clip.length;
+            animationEndEvent.functionName = "AnimationCompletedHandler";
+            animationEndEvent.stringParameter = clip.name;
+
+            clip.AddEvent(animationStartEvent);
+            clip.AddEvent(animationEndEvent);
+        }
+    }
+
+    public void AnimationStartHandler(string name)
+    {
+        Debug.Log($"{name} animation start.");
+    }
+
+    public void AnimationCompletedHandler(string name)
+    {
+        Debug.Log($"{name} animation end.");
+    }
+    #endregion
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _animator.Play("Shown");
+    }
+
+  
+}
