2775ae471f8c5a48f7f227f487a8f6f1fbdcc6df
ZionFrost <inaki337@gmail.com>
2023-03-16 17:30:14 +0100

    ADD TileView and BoardView

--- Assets/Scripts/MemoryGame.cs
+++ Assets/Scripts/MemoryGame.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class MemoryGame : MonoBehaviour
 {
     private MemoryBoard _board;
+    [SerializeField]
+    private GameObject _tilePrefab;
+    [SerializeField] 
+    private GameObject _memoryBoard;
+
+
+
+    
+
     private float _currentTime;
 
 
@@ -15,30 +24,18 @@ public class MemoryGame : MonoBehaviour
     {
         _board = new MemoryBoard(3, 3);
 
-        Tile tile = _board.Tiles[Random.Range(0, _board.Tiles.Count)];
-        tile.PropertyChanged += Tile_PropertyChanged;
+        _memoryBoard.GetComponent<MemoryBoardView>().SetUpMemoryBoardView(_board, _tilePrefab);
+
+        //Tile tile = _board.Tiles[Random.Range(0, _board.Tiles.Count)];
+        //tile.PropertyChanged += Tile_PropertyChanged;
     }
 
-    private void Tile_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-    {
-        Debug.Log($"Tile property '{e.PropertyName}' changed: {sender.ToString()}");
-    }
+    //private void Tile_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    //{
+    //    Debug.Log($"Tile property '{e.PropertyName}' changed: {sender.ToString()}");
+    //}
 
 
 
 
-    void Update()
-    {
-       
-        _currentTime += Time.deltaTime;
-
-        if (_currentTime > 2)
-        {
-            foreach (var tile in _board.Tiles)
-            {
-                tile.MemoryCardId++;
-            }
-            _currentTime -= 2;
-        }
-    }
 }
---
+++ Assets/Scripts/TileView.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TileView : ViewBaseClass<Tile> , IPointerDownHandler
+{
+
+
+
+
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    protected override void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+   
+    }
+
+   
+}
